50bba03557a5c8d69ef497802ccbc74a4f07f4f2
Niccolò Cuartas <cuartas.niccolo@gmail.com>
2020-11-23 12:37:16 +0100

    correzioni bug (tentativo)

---
+++ ASDL2021/src/it/unicam/cs/asdl2021/mp1/BalancedParenthesesCheckerTestDue.java
@@ -0,0 +1,318 @@
+package it.unicam.cs.asdl2021.mp1;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BalancedParenthesesCheckerTestDue {
+
+    @Test
+    void checkValidStrings() {
+        BalancedParenthesesChecker testing = new BalancedParenthesesChecker();
+
+        assertTrue(testing.check(""));
+        assertTrue(testing.check(" "));
+        assertTrue(testing.check("\t"));
+        assertTrue(testing.check("\n"));
+
+        assertTrue(testing.check("{}"));
+        assertTrue(testing.check("[]"));
+        assertTrue(testing.check("()"));
+
+        assertTrue(testing.check("{ }"));
+        assertTrue(testing.check("{\t}"));
+        assertTrue(testing.check("{\n}"));
+
+        assertTrue(testing.check("[ ]"));
+        assertTrue(testing.check("[\t]"));
+        assertTrue(testing.check("[\n]"));
+
+        assertTrue(testing.check("( )"));
+        assertTrue(testing.check("(\t)"));
+        assertTrue(testing.check("(\n)"));
+
+        assertTrue(testing.check("\t{}"));
+        assertTrue(testing.check("\t[]"));
+        assertTrue(testing.check("\t()"));
+        assertTrue(testing.check("\t {}"));
+        assertTrue(testing.check("\t ()"));
+        assertTrue(testing.check("\t []"));
+        assertTrue(testing.check("\t{ }"));
+        assertTrue(testing.check("\t[ ]"));
+        assertTrue(testing.check("\t( )"));
+        assertTrue(testing.check(" \t{}"));
+        assertTrue(testing.check(" \t[]"));
+        assertTrue(testing.check(" \t()"));
+        assertTrue(testing.check("\t{} "));
+        assertTrue(testing.check("\t[] "));
+        assertTrue(testing.check("\t() "));
+        assertTrue(testing.check(" \t{} "));
+        assertTrue(testing.check(" \t[] "));
+        assertTrue(testing.check(" \t() "));
+        assertTrue(testing.check("\t { }"));
+        assertTrue(testing.check("\t [ ]"));
+        assertTrue(testing.check("\t ( )"));
+        assertTrue(testing.check(" \t { } "));
+        assertTrue(testing.check(" \t [ ] "));
+        assertTrue(testing.check(" \t ( ) "));
+
+        assertTrue(testing.check("\n{}"));
+        assertTrue(testing.check("\n[]"));
+        assertTrue(testing.check("\n()"));
+        assertTrue(testing.check("\n {}"));
+        assertTrue(testing.check("\n ()"));
+        assertTrue(testing.check("\n []"));
+        assertTrue(testing.check("\n{ }"));
+        assertTrue(testing.check("\n[ ]"));
+        assertTrue(testing.check("\n( )"));
+        assertTrue(testing.check(" \n{}"));
+        assertTrue(testing.check(" \n[]"));
+        assertTrue(testing.check(" \n()"));
+        assertTrue(testing.check("\n{} "));
+        assertTrue(testing.check("\n[] "));
+        assertTrue(testing.check("\n() "));
+        assertTrue(testing.check(" \n{} "));
+        assertTrue(testing.check(" \n[] "));
+        assertTrue(testing.check(" \n() "));
+        assertTrue(testing.check("\n { }"));
+        assertTrue(testing.check("\n [ ]"));
+        assertTrue(testing.check("\n ( )"));
+        assertTrue(testing.check(" \n { } "));
+        assertTrue(testing.check(" \n [ ] "));
+        assertTrue(testing.check(" \n ( ) "));
+
+        assertTrue(testing.check("\n\t "));
+        assertTrue(testing.check("\n \t"));
+        assertTrue(testing.check(" \n\t"));
+
+        assertTrue(testing.check(" \t\n"));
+        assertTrue(testing.check("\t\n "));
+        assertTrue(testing.check("\t \n"));
+
+        assertTrue(testing.check("{}[]()"));
+        assertTrue(testing.check("{[()]}"));
+        assertTrue(testing.check("(()\t()\t())"));
+        assertTrue(testing.check("(()\n()\n())"));
+
+        assertTrue(testing.check("(()\t()\t())"));
+        assertTrue(testing.check("(()\t()\t[])"));
+        assertTrue(testing.check("(()\t()\t{})"));
+        assertTrue(testing.check("(()\t[]\t())"));
+        assertTrue(testing.check("(()\t[]\t[])"));
+        assertTrue(testing.check("(()\t[]\t{})"));
+        assertTrue(testing.check("(()\t{}\t())"));
+        assertTrue(testing.check("(()\t{}\t[])"));
+        assertTrue(testing.check("(()\t{}\t{})"));
+        assertTrue(testing.check("([]\t()\t())"));
+        assertTrue(testing.check("([]\t()\t[])"));
+        assertTrue(testing.check("([]\t()\t{})"));
+        assertTrue(testing.check("([]\t[]\t())"));
+        assertTrue(testing.check("([]\t[]\t[])"));
+        assertTrue(testing.check("([]\t[]\t{})"));
+        assertTrue(testing.check("([]\t{}\t())"));
+        assertTrue(testing.check("([]\t{}\t[])"));
+        assertTrue(testing.check("([]\t{}\t{})"));
+        assertTrue(testing.check("({}\t()\t())"));
+        assertTrue(testing.check("({}\t()\t[])"));
+        assertTrue(testing.check("({}\t()\t{})"));
+        assertTrue(testing.check("({}\t[]\t())"));
+        assertTrue(testing.check("({}\t[]\t[])"));
+        assertTrue(testing.check("({}\t[]\t{})"));
+        assertTrue(testing.check("({}\t{}\t())"));
+        assertTrue(testing.check("({}\t{}\t[])"));
+        assertTrue(testing.check("({}\t{}\t{})"));
+
+        assertTrue(testing.check("(()\t()\n())"));
+        assertTrue(testing.check("(()\t()\n[])"));
+        assertTrue(testing.check("(()\t()\n{})"));
+        assertTrue(testing.check("(()\t[]\n())"));
+        assertTrue(testing.check("(()\t[]\n[])"));
+        assertTrue(testing.check("(()\t[]\n{})"));
+        assertTrue(testing.check("(()\t{}\n())"));
+        assertTrue(testing.check("(()\t{}\n[])"));
+        assertTrue(testing.check("(()\t{}\n{})"));
+        assertTrue(testing.check("([]\t()\n())"));
+        assertTrue(testing.check("([]\t()\n[])"));
+        assertTrue(testing.check("([]\t()\n{})"));
+        assertTrue(testing.check("([]\t[]\n())"));
+        assertTrue(testing.check("([]\t[]\n[])"));
+        assertTrue(testing.check("([]\t[]\n{})"));
+        assertTrue(testing.check("([]\t{}\n())"));
+        assertTrue(testing.check("([]\t{}\n[])"));
+        assertTrue(testing.check("([]\t{}\n{})"));
+        assertTrue(testing.check("({}\t()\n())"));
+        assertTrue(testing.check("({}\t()\n[])"));
+        assertTrue(testing.check("({}\t()\n{})"));
+        assertTrue(testing.check("({}\t[]\n())"));
+        assertTrue(testing.check("({}\t[]\n[])"));
+        assertTrue(testing.check("({}\t[]\n{})"));
+        assertTrue(testing.check("({}\t{}\n())"));
+        assertTrue(testing.check("({}\t{}\n[])"));
+        assertTrue(testing.check("({}\t{}\n{})"));
+
+        assertTrue(testing.check("(()\n()\t())"));
+        assertTrue(testing.check("(()\n()\t[])"));
+        assertTrue(testing.check("(()\n()\t{})"));
+        assertTrue(testing.check("(()\n[]\t())"));
+        assertTrue(testing.check("(()\n[]\t[])"));
+        assertTrue(testing.check("(()\n[]\t{})"));
+        assertTrue(testing.check("(()\n{}\t())"));
+        assertTrue(testing.check("(()\n{}\t[])"));
+        assertTrue(testing.check("(()\n{}\t{})"));
+        assertTrue(testing.check("([]\n()\t())"));
+        assertTrue(testing.check("([]\n()\t[])"));
+        assertTrue(testing.check("([]\n()\t{})"));
+        assertTrue(testing.check("([]\n[]\t())"));
+        assertTrue(testing.check("([]\n[]\t[])"));
+        assertTrue(testing.check("([]\n[]\t{})"));
+        assertTrue(testing.check("([]\n{}\t())"));
+        assertTrue(testing.check("([]\n{}\t[])"));
+        assertTrue(testing.check("([]\n{}\t{})"));
+        assertTrue(testing.check("({}\n()\t())"));
+        assertTrue(testing.check("({}\n()\t[])"));
+        assertTrue(testing.check("({}\n()\t{})"));
+        assertTrue(testing.check("({}\n[]\t())"));
+        assertTrue(testing.check("({}\n[]\t[])"));
+        assertTrue(testing.check("({}\n[]\t{})"));
+        assertTrue(testing.check("({}\n{}\t())"));
+        assertTrue(testing.check("({}\n{}\t[])"));
+        assertTrue(testing.check("({}\n{}\t{})"));
+
+        assertTrue(testing.check("(()\n()\n())"));
+        assertTrue(testing.check("(()\n()\n[])"));
+        assertTrue(testing.check("(()\n()\n{})"));
+        assertTrue(testing.check("(()\n[]\n())"));
+        assertTrue(testing.check("(()\n[]\n[])"));
+        assertTrue(testing.check("(()\n[]\n{})"));
+        assertTrue(testing.check("(()\n{}\n())"));
+        assertTrue(testing.check("(()\n{}\n[])"));
+        assertTrue(testing.check("(()\n{}\n{})"));
+        assertTrue(testing.check("([]\n()\n())"));
+        assertTrue(testing.check("([]\n()\n[])"));
+        assertTrue(testing.check("([]\n()\n{})"));
+        assertTrue(testing.check("([]\n[]\n())"));
+        assertTrue(testing.check("([]\n[]\n[])"));
+        assertTrue(testing.check("([]\n[]\n{})"));
+        assertTrue(testing.check("([]\n{}\n())"));
+        assertTrue(testing.check("([]\n{}\n[])"));
+        assertTrue(testing.check("([]\n{}\n{})"));
+        assertTrue(testing.check("({}\n()\n())"));
+        assertTrue(testing.check("({}\n()\n[])"));
+        assertTrue(testing.check("({}\n()\n{})"));
+        assertTrue(testing.check("({}\n[]\n())"));
+        assertTrue(testing.check("({}\n[]\n[])"));
+        assertTrue(testing.check("({}\n[]\n{})"));
+        assertTrue(testing.check("({}\n{}\n())"));
+        assertTrue(testing.check("({}\n{}\n[])"));
+        assertTrue(testing.check("({}\n{}\n{})"));
+
+        assertTrue(testing.check("(( )\t()\n())"));
+        assertTrue(testing.check("(( )\t()\n[])"));
+        assertTrue(testing.check("(( )\t()\n{})"));
+        assertTrue(testing.check("(( )\t[]\n())"));
+        assertTrue(testing.check("(( )\t[]\n[])"));
+        assertTrue(testing.check("(( )\t[]\n{})"));
+        assertTrue(testing.check("(( )\t{}\n())"));
+        assertTrue(testing.check("(( )\t{}\n[])"));
+        assertTrue(testing.check("(( )\t{}\n{})"));
+        assertTrue(testing.check("([ ]\t()\n())"));
+        assertTrue(testing.check("([ ]\t()\n[])"));
+        assertTrue(testing.check("([ ]\t()\n{})"));
+        assertTrue(testing.check("([ ]\t[]\n())"));
+        assertTrue(testing.check("([ ]\t[]\n[])"));
+        assertTrue(testing.check("([ ]\t[]\n{})"));
+        assertTrue(testing.check("([ ]\t{}\n())"));
+        assertTrue(testing.check("([ ]\t{}\n[])"));
+        assertTrue(testing.check("([ ]\t{}\n{})"));
+        assertTrue(testing.check("({ }\t()\n())"));
+        assertTrue(testing.check("({ }\t()\n[])"));
+        assertTrue(testing.check("({ }\t()\n{})"));
+        assertTrue(testing.check("({ }\t[]\n())"));
+        assertTrue(testing.check("({ }\t[]\n[])"));
+        assertTrue(testing.check("({ }\t[]\n{})"));
+        assertTrue(testing.check("({ }\t{}\n())"));
+        assertTrue(testing.check("({ }\t{}\n[])"));
+        assertTrue(testing.check("({ }\t{}\n{})"));
+
+        assertTrue(testing.check("(()\t( )\n())"));
+        assertTrue(testing.check("(()\t( )\n[])"));
+        assertTrue(testing.check("(()\t( )\n{})"));
+        assertTrue(testing.check("(()\t[ ]\n())"));
+        assertTrue(testing.check("(()\t[ ]\n[])"));
+        assertTrue(testing.check("(()\t[ ]\n{})"));
+        assertTrue(testing.check("(()\t{ }\n())"));
+        assertTrue(testing.check("(()\t{ }\n[])"));
+        assertTrue(testing.check("(()\t{ }\n{})"));
+        assertTrue(testing.check("([]\t( )\n())"));
+        assertTrue(testing.check("([]\t( )\n[])"));
+        assertTrue(testing.check("([]\t( )\n{})"));
+        assertTrue(testing.check("([]\t[ ]\n())"));
+        assertTrue(testing.check("([]\t[ ]\n[])"));
+        assertTrue(testing.check("([]\t[ ]\n{})"));
+        assertTrue(testing.check("([]\t{ }\n())"));
+        assertTrue(testing.check("([]\t{ }\n[])"));
+        assertTrue(testing.check("([]\t{ }\n{})"));
+        assertTrue(testing.check("({}\t( )\n())"));
+        assertTrue(testing.check("({}\t( )\n[])"));
+        assertTrue(testing.check("({}\t( )\n{})"));
+        assertTrue(testing.check("({}\t[ ]\n())"));
+        assertTrue(testing.check("({}\t[ ]\n[])"));
+        assertTrue(testing.check("({}\t[ ]\n{})"));
+        assertTrue(testing.check("({}\t{ }\n())"));
+        assertTrue(testing.check("({}\t{ }\n[])"));
+        assertTrue(testing.check("({}\t{ }\n{})"));
+
+        assertTrue(testing.check("(()\n()\n( ))"));
+        assertTrue(testing.check("(()\n()\n[ ])"));
+        assertTrue(testing.check("(()\n()\n{ })"));
+        assertTrue(testing.check("(()\n[]\n( ))"));
+        assertTrue(testing.check("(()\n[]\n[ ])"));
+        assertTrue(testing.check("(()\n[]\n{ })"));
+        assertTrue(testing.check("(()\n{}\n( ))"));
+        assertTrue(testing.check("(()\n{}\n[ ])"));
+        assertTrue(testing.check("(()\n{}\n{ })"));
+        assertTrue(testing.check("([]\n()\n( ))"));
+        assertTrue(testing.check("([]\n()\n[ ])"));
+        assertTrue(testing.check("([]\n()\n{ })"));
+        assertTrue(testing.check("([]\n[]\n( ))"));
+        assertTrue(testing.check("([]\n[]\n[ ])"));
+        assertTrue(testing.check("([]\n[]\n{ })"));
+        assertTrue(testing.check("([]\n{}\n( ))"));
+        assertTrue(testing.check("([]\n{}\n[ ])"));
+        assertTrue(testing.check("([]\n{}\n{ })"));
+        assertTrue(testing.check("({}\n()\n( ))"));
+        assertTrue(testing.check("({}\n()\n[ ])"));
+        assertTrue(testing.check("({}\n()\n{ })"));
+        assertTrue(testing.check("({}\n[]\n( ))"));
+        assertTrue(testing.check("({}\n[]\n[ ])"));
+        assertTrue(testing.check("({}\n[]\n{ })"));
+        assertTrue(testing.check("({}\n{}\n( ))"));
+        assertTrue(testing.check("({}\n{}\n[ ])"));
+        assertTrue(testing.check("({}\n{}\n{ })"));
+
+        assertTrue(testing.check("( ([{}\t(\n \t) [\t\t]] )\n )"));
+        assertTrue(testing.check("( ([{ \t}(\n\n\n){ \t}(\n \t) {[]}[\t\t]] )\n )"));
+        assertTrue(testing.check("( ([{ \t}(\n\n\n){ \t}(\n \t) {\n[( \t []\n( \t\n {}[ \n]))]}[\t\t]] )\n )"));
+        assertTrue(testing.check("( ([{}\t(\n \t) [\t()\n[ ]({}) \n([\n()\t[\n]()[\n\n\n\n\t\n\t (\n[\t])]])\t]] )\n )"));
+        assertTrue(testing.check("( ([{}\t(\n \t) (){ [\n]\t}[\t(  [\n])\n[ ]({()[\t]}) \n([\n()\t[\n]()[\n\n\n\n\t\n\t (\n[\t])]])\t]] )\n )"));
+    }
+
+    @Test
+    void checkNonValidStrings(){
+        BalancedParenthesesChecker testing = new BalancedParenthesesChecker();
+        assertThrows(IllegalArgumentException.class, ()->testing.check("https://youtu.be/05ezsaFPM14"));
+        assertFalse(testing.check("("));
+        assertFalse(testing.check(")"));
+        assertFalse(testing.check("["));
+        assertFalse(testing.check(")"));
+        assertFalse(testing.check("{"));
+        assertFalse(testing.check("}"));
+        assertFalse(testing.check("}{"));
+        assertFalse(testing.check("(("));
+        assertFalse(testing.check("[["));
+        assertFalse(testing.check("[}"));
+        assertFalse(testing.check("(((({}[\t])))))\n"));
+    }
+}
